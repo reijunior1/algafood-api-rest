4d5ef0f7461febd8ae5ceb891c95976e16f62c7d
reijunior1 <reijunior1@hotmail.com>
2020-06-26 22:02:37 -0300

    Consultando-banco-de-dados
    Consultando objetos do banco de dados

---
+++ src/main/java/com/algaworks/algafood/api/jpa/CadastroCozinha.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.api.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.api.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public List<Cozinha> listar() {
+		return manager.createQuery("from Cozinha", Cozinha.class)
+				.getResultList();
+	}
+	
+}
