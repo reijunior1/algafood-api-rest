b5b7d3c9dcb58f3d5836f374689263a9fdacfd66
reijunior1 <reijunior1@hotmail.com>
2020-06-26 22:50:02 -0300

    Buscando-objeto pelo-id-no-banco-de-dados

---
+++ src/main/java/com/algaworks/algafood/api/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.api.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.api.AlgafoodApiRestApplication;
+import com.algaworks.algafood.api.domain.model.Cozinha;
+
+public class BuscaCozinhaMain {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiRestApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha cozinha = cadastroCozinha.buscar(1L);
+		
+			System.out.println(cozinha.getNome());
+		}
+		
+	}
+
+
