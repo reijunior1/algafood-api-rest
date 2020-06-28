fa268ab0ab1cc6bcb6401f7ba3b8c18517881699
reijunior1 <reijunior1@hotmail.com>
2020-06-28 20:17:56 -0300

    nullable-@Column-@JoinColumn

--- src/main/java/com/algaworks/algafood/api/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/api/domain/model/Cozinha.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.domain.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,10 +17,9 @@ public class Cozinha {
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
 	private Long id;
 	
-	
+	@Column(nullable = false)
 	private String nome;	
 	
 }
